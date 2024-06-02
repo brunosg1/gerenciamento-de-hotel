970b6f90b2e2dc6b113c111fae1918187fc9869e
brunosg1 <bruno.silva_gomes@hotmail.com>
2024-06-02 20:30:13 -0300

    Initial commit

---
+++ Program.cs
@@ -0,0 +1,10 @@
+﻿namespace Projeto_UML
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello, World!");
+        }
+    }
+}
