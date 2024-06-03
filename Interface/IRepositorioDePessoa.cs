f933c44795bb51b04bc48c7dfc9d5a66f5e75c04
brunosg1 <bruno.silva_gomes@hotmail.com>
2024-06-02 21:05:25 -0300

    Add Pessoa
    + funcionario, hospede, pessoa, PessoaFactory, IRepositorioDePessoa, RepositorioDePessoa

---
+++ Interface/IRepositorioDePessoa.cs
@@ -0,0 +1,18 @@
+﻿using Projeto_UML.Class;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Projeto_UML.Interface
+{
+    public interface IRepositorioPessoa
+    {
+        void Inserir(Pessoa pessoa);
+        void Atualizar(Pessoa pessoa);
+        void Excluir(Pessoa pessoa);
+        Pessoa ObterPorId(int id);
+        List<Pessoa> ObterTodos();
+    }
+}
---
+++ Repositorio/RepositorioDePessoa.cs
@@ -0,0 +1,44 @@
+﻿using Projeto_UML.Class;
+using Projeto_UML.Interface;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Projeto_UML.Repositorio
+{
+    public class RepositorioPessoa : IRepositorioPessoa
+    {
+        private List<Pessoa> pessoas = new List<Pessoa>();
+
+        public void Inserir(Pessoa pessoa)
+        {
+            pessoas.Add(pessoa);
+        }
+
+        public void Atualizar(Pessoa pessoa)
+        {
+            var index = pessoas.FindIndex(p => p.Id == pessoa.Id);
+            if (index != -1)
+            {
+                pessoas[index] = pessoa;
+            }
+        }
+
+        public void Excluir(Pessoa pessoa)
+        {
+            pessoas.Remove(pessoa);
+        }
+
+        public Pessoa ObterPorId(int id)
+        {
+            return pessoas.FirstOrDefault(p => p.Id == id);
+        }
+
+        public List<Pessoa> ObterTodos()
+        {
+            return pessoas;
+        }
+    }
+}
